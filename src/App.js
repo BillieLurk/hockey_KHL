fe9601213e79d92935a87ab82e2c85ae14610796
BillieLurk <bill@1618holding.com>
2025-11-27 15:00:35 +0100

    things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ScoreBar from "./components/ScoreBar";
 
 function App() {
   return (
-    <div className="App" style={{ opacity: 0.3 }}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <ScoreBar />
     </div>
   );
 }
---
+++ src/components/ScoreBar.jsx
@@ -0,0 +1,35 @@
+const ScoreBar = () => {
+  return (
+    <div className="fixed bottom-0 w-full flex justify-center items-center">
+      <div className="bg-black h-[100px] w-2/4 flex items-center">
+        {/* Left player column */}
+        <div className="flex-1 flex justify-center items-center">
+          <Player />
+        </div>
+
+        {/* Center image column */}
+        <div className="flex-shrink-0">
+          <img src="/khl.png" alt="cheese" className="h-full" />
+        </div>
+
+        {/* Right player column */}
+        <div className="flex-1 flex justify-center items-center">
+          <Player right />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Player = ({ right = false }) => {
+  return (
+    <div
+      className={`text-[40px] text-white ${right && "[&>*]:text-right flex flex-row-reverse"}`}
+    >
+      <input value={"player name"} className="bg-transparent w-fit" />
+      <span>0</span>
+    </div>
+  );
+};
+
+export default ScoreBar;
